7629681c5aa860bf0380afad12926d4d46dbd221
Stefano Bonfanti <sbonfanti@hotmail.com>
2026-06-18 12:05:09 +0200

    fix: add ScrollToTop component for better navigation

--- app/src/components/layout/Layout.tsx
+++ app/src/components/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 
 export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-bone">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow pt-24 md:pt-32">
         <Outlet />
